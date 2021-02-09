efe75fe1d261da72dc8d82ed459f1486c9c0c286
Tumelo <tumie201511@gmail.com>
2021-02-09 23:39:20 +0200

    updated Header.js file

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header() {
     return (
         <div className="header">
-            <img className="header_logo" alt="" src={process.env.PUBLIC_URL + "/1200px-Instagram_logo.svg.png"} />
+            <img className="header__logo" alt="" src={process.env.PUBLIC_URL + "/1200px-Instagram_logo.svg.png"} />
          
         </div>
     )
