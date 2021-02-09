a47833a921a5d154fb70ac13318936d204ce28d9
Tumelo <tumie201511@gmail.com>
2021-02-09 23:50:18 +0200

    updated App js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header/Header'
+import Posts from './Posts/Posts'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Posts />
       
     </div>
   );
