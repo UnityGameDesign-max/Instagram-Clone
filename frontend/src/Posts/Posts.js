3371d15ae16bd948c6dc94ee7632385ff83d5ccf
Tumelo <tumie201511@gmail.com>
2021-02-10 16:08:49 +0200

    updated Post js file

--- frontend/src/Posts/Posts.js
+++ frontend/src/Posts/Posts.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Posts.css'
 import Avatar from '@material-ui/core/Avatar'
 
-function Posts() {
+function Posts({post__img, username, caption, avatar__alt}) {
+
     return (
         <div className="posts">
             <div className="post__avatar__title">
                 
-                <Avatar className="post__avatar" alt="Tumi" src="/static/images/avatar/1.jpg" />
-                <h3>Username</h3>
+                <Avatar className="post__avatar" alt={avatar__alt} src="/static/images/avatar/1.jpg" />
+                <h3>{username}</h3>
             </div>
             
-            <img className="post__img" alt="" src={process.env.PUBLIC_URL + "trends.jpg"} />
+            <img className="post__img" alt="" src={process.env.PUBLIC_URL + post__img} />
             
-            <h4 className="post__text"><strong>Tumelo</strong> These are exciting style trends!</h4>
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
         </div>
     )
 }
