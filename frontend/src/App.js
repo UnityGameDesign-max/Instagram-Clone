2693ae839e55ae3b2edc8a9d2d3c0367ea8cadb9
Tumelo <tumie201511@gmail.com>
2021-02-10 16:07:58 +0200

    updated App js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
+import React, { useState } from 'react'
 import './App.css';
 import Header from './Header/Header'
 import Posts from './Posts/Posts'
 
 function App() {
+  const [posts, setPosts] = useState([
+
+    {
+      username: "Tumelo",
+      captions: "what a wonderful day!",
+      avatar__alt: "Tumelo",
+      post__img: "trends.jpg"
+    },
+
+    {
+      username: "Katleho",
+      captions: "When we still young!",
+      avatar__alt: "Katleho",
+      post__img: "fancy_cat.jpg"
+    },
+
+
+    {
+      username: "Cars.com",
+      captions: "most powerful and beautiful!",
+      avatar__alt: "Cars.com",
+      post__img: "ford_mustang.jpg"
+    }
+  ]);
+
+
   return (
     <div className="App">
       <Header />
-      <Posts />
+      {
+        posts.map(post =>(
+          <Posts username={post.username} avatar__alt={post.avatar__alt} caption={post.captions} post__img={post.post__img}/>
+        ))
+      }
       
     </div>
   );
