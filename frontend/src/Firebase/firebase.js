83e2a40eaaad841d8ec7b3c5fcd78af5b58bb777
Tumelo <tumie201511@gmail.com>
2021-02-14 16:04:37 +0200

    added Firebase folders in our projects

---
+++ frontend/src/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyArGMAyTql2bmTxZaBLyjQSde-qWXg1efg",
+    authDomain: "instagram-clone-3ca11.firebaseapp.com",
+    databaseURL: "https://instagram-clone-3ca11-default-rtdb.firebaseio.com",
+    projectId: "instagram-clone-3ca11",
+    storageBucket: "instagram-clone-3ca11.appspot.com",
+    messagingSenderId: "579070667538",
+    appId: "1:579070667538:web:7fbf606f61d8247957a1cb",
+    measurementId: "G-H1DXDE758J"
+  };
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
