f937dacf3b13d0ffdedb28754407463881a6a56c
Tumelo <tumie201511@gmail.com>
2021-02-16 02:44:52 +0200

    updated Header js file

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Header.css'
+import { Button} from '@material-ui/core'
+import { auth } from '../Firebase/firebase'
 
-function Header() {
+function Header({user, setOpen, setOpenSignIn}) {
     return(
         <div className="header">
-            <img className="header__logo" alt="" src={process.env.PUBLIC_URL + "/1200px-Instagram_logo.svg.png"} />
+
+        <img className="header__logo" alt="" src={process.env.PUBLIC_URL + "/1200px-Instagram_logo.svg.png"} />
+
+        <div className="signIn_signOut_button">
+            {user ? (<Button onClick={() => auth.signOut()}>Logout</Button>): (
+                <Button onClick={() => setOpen(true)}>Sign Up</Button>
+            )}
+            {!user?(<Button onClick={() => setOpenSignIn(true)} >LogIn</Button>): (
+                <Button onClick={()=> setOpen(true)}>SignUp</Button>
+            )}
+        </div>
+        
+        
         </div>
     )
 }
