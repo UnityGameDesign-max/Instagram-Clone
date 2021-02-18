5e2aad7d0ca48de12e45af62ea9542db5ab08a59
Tumelo <tumie201511@gmail.com>
2021-02-18 15:13:29 +0200

    updated Header js file

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import './Header.css'
 import { Button} from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home';
+import ExploreOutlinedIcon from '@material-ui/icons/ExploreOutlined';
 import { auth } from '../Firebase/firebase'
+
 
 function Header({user, setOpen, setOpenSignIn}) {
     return(
         <div className="header">
 
-        <img className="header__logo" alt="" src={process.env.PUBLIC_URL + "/1200px-Instagram_logo.svg.png"} />
+            <img className="header__logo" alt="" src={process.env.PUBLIC_URL + "/1200px-Instagram_logo.svg.png"} />
+            <div className="signIn_signOut_button">
 
-        <div className="signIn_signOut_button">
-            {user ? (<Button onClick={() => auth.signOut()}>Logout</Button>): (
-                <Button onClick={() => setOpen(true)}>Sign Up</Button>
-            )}
-            {!user?(<Button onClick={() => setOpenSignIn(true)} >LogIn</Button>): (
-                <Button onClick={()=> setOpen(true)}>SignUp</Button>
-            )}
+            <div className="navigation__list">
+                <HomeIcon className="home__logo" />
+                <ExploreOutlinedIcon />
+                {user ? (<Button onClick={() => auth.signOut()}>Logout</Button>): (
+                    <Button onClick={() => setOpen(true)}>Sign Up</Button>
+                )}
+                {!user?(<Button onClick={() => setOpenSignIn(true)} >LogIn</Button>): (
+                    <Button onClick={()=> setOpen(true)}>SignUp</Button>
+                )}
+            </div>
+            
+            
         </div>
         
         
