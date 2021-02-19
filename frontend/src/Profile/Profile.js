6aa1fa6f8cbad9ef454404b3852b8b3a1ad9ded3
Tumelo <tumie201511@gmail.com>
2021-02-19 15:34:58 +0200

    added new Profile folder

---
+++ frontend/src/Profile/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import './Profile.css'
+
+function Profile({user}) {
+    return (
+        <div className="profile">
+
+           
+            <div className="avatar__profile">
+                <div className="friend__suggestions">
+                     
+                    <div className="user">
+                        <Avatar />
+                        <div className="user__details">
+                            <h5>Lehlohonolo Mopeli</h5>
+                            <small>Follows you</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="follow">
+                    
+                    <a href="#">Follow</a>
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Profile
